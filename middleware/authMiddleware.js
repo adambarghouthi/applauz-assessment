a07d14ed78e7357aee09e9ee77ee2cbb0ab5272c
adambarghouthi <adambargh@gmail.com>
2020-11-03 18:52:28 -0500

    middleware to check api key

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
-function authMiddleware(req, res) {
-  const req.headers.authentication
+function authMiddleware(req, res, next) {
+  const { authentication } = req.headers
 
   if (authentication !== 'secret') {
     return res.status(403).json({ error: 'Unauthorized request.' })
