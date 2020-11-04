786b2faeda801017d7631bdc1a0c9540e5594047
adambarghouthi <adambargh@gmail.com>
2020-11-04 12:42:03 -0500

    move res & db handlers to helpers folder

--- api/users.js
+++ api/users.js
@@ -1,35 +1,6 @@
-const fs = require('fs')
 
-// json file read/write functions
-// similar to db connection
-function readFile(cb) {
-  fs.readFile('./db/users.json', function (err, data) {
-    if (err) cb(err, null)
-    else cb(null, JSON.parse(data))
-  })
-}
-
-function writeFile(cb, data) {
-  fs.writeFile('./db/users.json', data, function (err) {
-    if (err) cb(err, null)
-    else cb(null, JSON.parse(data))
-  })
-}
-
-// Success & error handlers
-function handleSuccess(data) {
-  return {
-    status: 'success',
-    body: data
-  }
-}
-
-function handleError(error) {
-  return {
-    status: 'error',
-    message: error
-  }
-}
+const { readFile, writeFile } = require('./helpers/dbHandler')
+const { handleSuccess, handleError } = require('./helpers/resHandler')
 
 // helper for get function
 // checks if value matches query term
