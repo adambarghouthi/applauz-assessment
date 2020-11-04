37dd93f0be9262c11774c6405449efb05ef15eee
adambarghouthi <adambargh@gmail.com>
2020-11-04 13:51:55 -0500

    mv to api folder

---
+++ api/middleware/authMiddleware.js
@@ -0,0 +1,13 @@
+const { handleError } = require('../helpers/resHandler')
+
+function authMiddleware(req, res, next) {
+  const { authentication } = req.headers
+
+  if (authentication !== 'secret') {
+    return res.status(403).json(handleError('Unauthorized request.'))
+  }
+
+  next()
+}
+
+module.exports = authMiddleware
